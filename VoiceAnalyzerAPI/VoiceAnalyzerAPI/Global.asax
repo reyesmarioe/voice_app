f823240f6eb48dbae220657a622310d224724779
raydi <raydiaz3000@gmail.com>
2023-09-26 22:12:32 -0700

    moved folder into voice_app

--- VoiceAnalyzerAPI/VoiceAnalyzerAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoiceAnalyzerAPI.WebApiApplication" Language="C#" %>
---
+++ VoiceAppRestAPI/VoiceAppRestAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VoiceAppRestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
