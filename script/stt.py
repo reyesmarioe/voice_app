257c969be97468d988bfa19bb1eb8a8a6c028cd0
Mario Emiliano Reyes <mario.reyes@gmail.com>
2023-10-22 18:49:36 -0700

    Generate stats and save to file

--- script/stt.py
+++ script/stt.py
@@ -8,10 +8,15 @@
 '''
 import speech_recognition as sr
 import os
+import sys
+import time
+import json
 
 
 class STT:
-    def __init__(self, audio_file):
+    def __init__(self, audio_file, text_file):
+        self.data = {}
+        self.text_file = text_file
         if (audio_file is None):
             print ('Please provide an audio file')
             return
@@ -25,21 +30,41 @@ class STT:
             return -1, None
 
 
+        startProcess = time.time()
 
         r = sr.Recognizer()
-        audio_file = sr.AudioFile('audio.wav')
+        audio_file = sr.AudioFile(self.audio_file)
 
         with audio_file as source:
             audio = r.record(source)
 
 
-        return 0, (r.recognize_google(audio))
+        data = r.recognize_google(audio)
+        endProcess = time.time()
+        if len(data) > 0: 
+            self.data['text'] = data
+            self.data['fileName'] = self.audio_file
+            self.data['wordCount'] = len(data.split(' '))
+            self.data['processingTime'] = '{:.2f} secs'.format(endProcess - startProcess)
+                    
+            return 0
+        else:
+            return 1
+
+    def write_to_file(self):
+        with open(self.text_file, 'w') as fp:
+            fp.write(json.dumps(self.data, indent=2))
 
 
 if __name__ == '__main__':
-    stt = STT('audio.wav')
 
-    res, data = stt.process_file()
+    if 3 > len(sys.argv):
+        print('stt.py <audio_file> <text_file>')
+        sys.exit() 
+
+    stt = STT(sys.argv[1], sys.argv[2])
+
+    res = stt.process_file()
 
     if 0 == res:
-        print(data)
+        stt.write_to_file()
