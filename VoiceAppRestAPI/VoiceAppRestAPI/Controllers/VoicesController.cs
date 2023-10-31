574de3cb47306bc2f3070d1299b872678ae80ae9
raydi <raydiaz3000@gmail.com>
2023-10-30 22:26:22 -0700

    latest updates

--- VoiceAppRestAPI/VoiceAppRestAPI/Controllers/VoicesController.cs
+++ VoiceAppRestAPI/VoiceAppRestAPI/Controllers/VoicesController.cs
@@ -11,7 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using System.Text;
 using Azure.Storage.Blobs;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 //using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace VoiceAppRestAPI.Controllers
 {
@@ -32,64 +35,112 @@ namespace VoiceAppRestAPI.Controllers
         // POST api/values
         [HttpPost]
         [Route("api/uploadvoice")]
-        public async Task<HttpResponseMessage> UploadVoiceFileBuffer(IFormFile file)
-        {
+        public async Task<IHttpActionResult> UploadVoiceFileBuffer() 
+        { 
             try
             {
-
-                if (file != null)
+                if (!Request.Content.IsMimeMultipartContent())
                 {
-                    if (!file.ContentType.Equals("audio/wav", StringComparison.OrdinalIgnoreCase))
-                    {
-                        return Request.CreateErrorResponse(HttpStatusCode.UnsupportedMediaType, "Unsupported Media Type");
+                    return BadRequest("The request is not a valid multipart/form-data request");
+                }
 
+                string outputStr = "";
+
+                var provider = new MultipartFormDataStreamProvider(Path.GetTempPath());
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                foreach (var fileData in provider.FileData) {
+
+                    var headers = fileData.Headers;
+
+                    string originalFileName = headers.ContentDisposition.FileName.Trim('\"');
+
+                    if (string.IsNullOrEmpty(originalFileName))
+                    {
+                        continue; // skip if filename not available
                     }
-               
+                    var file = new FileInfo(fileData.LocalFileName);
 
                     string sasToken = "sv=2021-10-04&ss=f&srt=co&se=2023-10-25T05%3A04%3A50Z&sp=rl&sig=5RepzlEp%2BvyTTKNQMhKmIeGLYbU2FJSryHLCf5B27OY%3D";
 
                     string blobUrlWithSAS = "https://cs410032001eacab1d7.file.core.windows.net/cs-rdiaz55-uisad-uis-edu-10032001eacab1d7/voices" + sasToken;
                     string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cs410032001eacab1d7;AccountKey=BGSQ3qn21WeD0mzUGCwZxA5mTbSHA+2Nyx+gCU8YY3p2kJtmuqytIhhHBRljt1NPKjmPynOlR32y+AStLcWPRQ==;EndpointSuffix=core.windows.net";
-                    
-                    string containerName = "cs-rdiaz55-uisad-uis-edu-10032001eacab1d7";
 
-                    var blobName = $"{Guid.NewGuid()}-{file.FileName}";
+                    string containerName = "testcontainer"; // "cs-rdiaz55-uisad-uis-edu-10032001eacab1d7";
 
-                    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
-                    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-                    BlobClient blobClient = containerClient.GetBlobClient(blobName);
+                    var blobName = $"{Guid.NewGuid()}-{originalFileName}";
 
-                    using (Stream stream = file.OpenReadStream())
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                    CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+                    CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+
+
+                    using (var fileStream = File.OpenRead(fileData.LocalFileName)) // Request.Content.ReadAsStreamAsync().Result
                     {
-                        await blobClient.UploadAsync(stream, true);
-                        var data = new { message = "Hello mate" };
-                        return Request.CreateResponse(HttpStatusCode.OK, new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json"));
+                        await blob.UploadFromStreamAsync(fileStream);
+
                     }
 
-                    /*
-                    var voiceFile = HttpContext.Current.Request.Files[0];
-                    if ((voiceFile == null) || (voiceFile.ContentLength == 0))
+                    // call python script against the file
+                    string pythonScript = "callStt.py";
+                    ProcessStartInfo startInfo = new ProcessStartInfo
                     {
-                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Voice file could not be uploaded.");
-                    }
-                    var voiceUploadFolder = AppDomain.CurrentDomain.BaseDirectory + "\\" + "UploadVoice";
-                    Console.WriteLine("Voice upload folder was:" + voiceUploadFolder);
-                    Directory.CreateDirectory(voiceUploadFolder);
+                        FileName = "python",
+                        Arguments = $"{pythonScript} {blobName}",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                    var voiceFileName = $"{voiceFile.FileName}_{Guid.NewGuid()}";
-                    var longFile = voiceUploadFolder + "\\" + voiceFileName;
-                    Console.WriteLine("Voice filename was:" + longFile);
-                    voiceFile.SaveAs(longFile);
-                    return Request.CreateResponse(HttpStatusCode.OK, "Voice file has been received:" + longFile.ToString()); */
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo = startInfo;
+
+                        process.OutputDataReceived += (sender, e) =>
+                        {
+                            if (!string.IsNullOrEmpty(e.Data))
+                            {
+                                // Handle the output from your Python script
+                                Console.WriteLine(e.Data);
+                            }
+                        };
+
+                        process.ErrorDataReceived += (sender, e) =>
+                        {
+                            if (!string.IsNullOrEmpty(e.Data))
+                            {
+                                // Handle any errors from your Python script
+                                Console.WriteLine("Error: " + e.Data);
+                                outputStr += ("Error: " + e.Data);
+                            }
+                        };
+
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        process.WaitForExit();
+
+                        // Check the exit code to see if the script was successful
+                        int exitCode = process.ExitCode;
+                        if (exitCode == 0)
+                        {
+                            Console.WriteLine("Python script executed successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Python script failed with exit code {exitCode}");
+                        }
+                    }
                 }
-                else
-                {
-                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Null file");
-                }
+
+                return Ok("File uploaded and analysis run successfully:" + outputStr);
+                
             }
             catch (Exception e)
             {
-                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error with upload:" + "" + e.Message);
+                return InternalServerError(e); //Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error with upload:" + "" + e.Message);
             }
 
         }
